9a6bc7db5f8e2c906a03c688455f72702e659b5f
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-08-06 13:23:20 -0500

    add code for main method

--- DSA/src/com/dsa/binarysearch/BinarySearchDemo.java
+++ DSA/src/com/dsa/binarysearch/BinarySearchDemo.java
@@ -1,4 +1,22 @@
 package com.dsa.binarysearch;
 
-public class BinarySearchDemo {
+public class BinarySearchDemo
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {11, 24, 53, 974, 42, 5, 6, 420}; // Array must be sorted.
+        int target = 42; // Value to search for.
+
+        int result = binarySearch(numbers, target); // Store result.
+
+        if(result > 0) // If element was found.
+            System.out.println("Target found at index: " + result); // Print result.
+        else // Element not found.
+            System.out.println("Target was not found in the array."); // Print not found message.
+    }
+
+    private static int binarySearch(int[] numbers, int target)
+    {
+        return -1;
+    }
 }
