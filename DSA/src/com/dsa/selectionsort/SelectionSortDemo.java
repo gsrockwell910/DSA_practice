7ce06d75efa534ef73ced7b8cb75cf286c0f4809
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-08-12 17:02:39 -0500

    Add code for main method

--- DSA/src/com/dsa/selectionsort/SelectionSortDemo.java
+++ DSA/src/com/dsa/selectionsort/SelectionSortDemo.java
@@ -2,4 +2,22 @@ package com.dsa.selectionsort;
 
 public class SelectionSortDemo
 {
+    public static void main(String[] arg)
+    {
+        int[] numbers = {6, 5, 2, 8, 9, 4};
+        int size = numbers.length;
+        int temp = 0;
+
+        System.out.println("Before sorting: ");
+        for(int num : numbers)
+        {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nAfter sorting: ");
+        for(int num : numbers)
+        {
+            System.out.print(num + " ");
+        }
+    }
 }
