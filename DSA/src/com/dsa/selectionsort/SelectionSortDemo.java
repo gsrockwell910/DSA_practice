3cb6a6e1f45f87c81678e7e6237b1377524f0132
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-08-12 19:08:45 -0500

    Add comments to code and make slight changes.

--- DSA/src/com/dsa/selectionsort/SelectionSortDemo.java
+++ DSA/src/com/dsa/selectionsort/SelectionSortDemo.java
@@ -1,45 +1,65 @@
 package com.dsa.selectionsort;
 
+/**
+ *  The SelectionSortDemo class demonstrates
+ *  the selectionSort method.
+ */
+
 public class SelectionSortDemo
 {
     public static void main(String[] arg)
     {
-        int[] numbers = {6, 5, 2, 8, 9, 4};
+        int[] numbers = {6, 5, 2, 8, 9, 4}; // Create array.
 
-        System.out.println("Before sorting: ");
-        for(int num : numbers)
+        System.out.println("Before sorting: "); // Output.
+        for(int num : numbers) // For each
         {
-            System.out.print(num + " ");
+            System.out.print(num + " "); // Print it out.
         }
 
-        selectionSort(numbers);
-        
-        System.out.println("\nAfter sorting: ");
-        for(int num : numbers)
+        selectionSort(numbers); // Call selectionSort method.
+
+        System.out.println("\nAfter sorting: "); // Output.
+        for(int num : numbers) // For each
         {
-            System.out.print(num + " ");
+            System.out.print(num + " "); // Print it out.
         }
     }
 
+    /**
+     *  The selectionSort method performs a selection sort operation
+     *  an array of numbers.
+     *
+     * @param numbers The array to sort.
+     */
     public static void selectionSort(int[] numbers)
     {
-        int size = numbers.length;
-        int temp = 0;
-        int minIndex = -1;
+        int size = numbers.length; // Store array size in variable.
+        int temp; // Temp for swapping.
+        int minIndex; // Keeps track of the index of the smallest number found in the array.
 
+        // First loop performs the swaps,
+        // goes thorough every valid index in the array.
         for(int i = 0; i < size - 1; i++)
         {
+            // Set minIndex to current index. moves forward one each iteration,
+            // sorting the smallest number to the front in this case.
             minIndex = i;
+
+            // Inner loop compares the number at the current index,
+            // with the number at current index + 1.
             for(int j = i + 1; j < size; j++)
             {
+                // If number at current index is greater than number at next index.
                 if(numbers[minIndex] > numbers[j])
                 {
-                    minIndex = j;
+                    minIndex = j; // Next index is stored as the minIndex;
                 }
             }
-            temp = numbers[minIndex];
+            temp = numbers[minIndex]; // Set temp to index of smallest number found.
+            // Swap the number at the index of the current smallest number for the number at the current index.
             numbers[minIndex] = numbers[i];
-            numbers[i] = temp;
+            numbers[i] = temp; // Set the number at the current index to the number at minIndex.
         }
     }
 }
